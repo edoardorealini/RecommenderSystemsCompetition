24eeff3089225e412f9efbc5e22ad96017cfdcaa
Stefano Formicola <stefano_17sf@libero.it>
2019-12-11 14:33:18 +0100

    setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='RecSys-Competition-2019',
+    version='',
+    packages=['Cython', 'h5py', 'nltk', 'nose', 'numpy', 'pandas', 'scikit-learn', 'scikit-optimize', 'scipy', 'tqdm'],
+    url='',
+    license='',
+    author='',
+    author_email='',
+    description=''
+)
